0d8cf58c2f8a8d18a933655c171130b4e9ba12f5
Mahtab Hossen Jhalok <48786676+Mahtabhj@users.noreply.github.com>
2022-07-27 02:37:45 +0600

    Fixed

---
+++ src/cfg/Main.java
@@ -0,0 +1,12 @@
+package cfg;
+
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		String path = "test.txt";
+		new Parser(path);
+	}
+
+}
